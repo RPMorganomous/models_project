4c7dabe24bd81847c982c0903a4aa34e9ca2b006
Richard Morgan <mba.rick@gmail.com>
2017-06-11 19:32:21 -0500

    Working query

--- models_app/views.py
+++ models_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from models_app.models import Topic,Webpage,AccessRecord
 
 # Create your views here.
 def index(request):
-    my_dict = {'insert_content':"HELLO I'M FROM MODELS_APP!"}
-    return render(request,'models_app_templ/index.html', context=my_dict)
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records':webpages_list}
+
+    # my_dict = {'insert_content':"HELLO I'M FROM MODELS_APP!"}
+    return render(request,'models_app_templ/index.html', context=date_dict)
